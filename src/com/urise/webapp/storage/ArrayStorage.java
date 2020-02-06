a5466220b4b52a5b941d6a99067a96e36580b757
chemike <chemike707@gamsil.com>
2020-02-06 16:16:32 +0800

    Introduce abstract storage and sorted storage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,10 +7,7 @@ import java.util.*;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage {
-    private static final int STORAGE_LIMIT = 10000;
-    private Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -35,15 +32,6 @@ public class ArrayStorage implements Storage {
         } else System.out.println("Resume is already present " + resume.getUuid());
     }
 
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (getIndex(uuid) != -1) {
-            return storage[index];
-        }
-        System.out.println("Resume not found " + uuid);
-        return null;
-    }
-
     public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
@@ -53,7 +41,7 @@ public class ArrayStorage implements Storage {
         } else System.out.println("Resume not found " + uuid);
     }
 
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
@@ -67,9 +55,5 @@ public class ArrayStorage implements Storage {
      */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
     }
 }

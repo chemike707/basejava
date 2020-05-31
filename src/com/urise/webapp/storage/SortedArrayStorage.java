a5b00551a57b6d3894e957d82ad0144f28273a37
chemike <chemike707@gamsil.com>
2020-05-31 18:48:56 +0800

    HW7 add generic

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -6,15 +6,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-//    ResumeComparator resumeComparator = new ResumeComparator();
-//
-//    private static class ResumeComparator implements Comparator<Resume> {
-//        @Override
-//        public int compare(Resume o1, Resume o2) {
-//            return o1.getUuid().compareTo(o2.getUuid());
-//        }
-//    }
-
     Comparator<Resume> comparator = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
 
     @Override
